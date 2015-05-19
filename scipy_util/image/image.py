417393d8d17940d192b1541e3488c22be095861c
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-05-19 20:30:32 +0800

    gaussian blur

--- scipy_util/image/image.py
+++ scipy_util/image/image.py
@@ -3,9 +3,9 @@ import numpy as np
 __author__ = 'Daniel'
 
 
-def mat2img(mat, img_name="Image Name"):
+def imshow(mat, img_name="Image Name"):
     """
-    Display the image and wait 
+    Display the image and wait
     :param mat: normally the data type is 8-bit int, for other cases:
     http://docs.opencv.org/modules/highgui/doc/user_interface.html?highlight=imshow#cv2.imshow
     :param img_name:
@@ -15,7 +15,27 @@ def mat2img(mat, img_name="Image Name"):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-if __name__=="__main__":
-    mat = np.random.randint(0, 255, (400, 400))
+
+def random_img(size=(400, 400)):
+    """
+    Gray scale image with random noise 
+    """
+    mat = np.random.randint(0, 255, size)
     mat = np.asarray(mat, dtype=np.int8)
-    mat2img(mat)
+    return mat
+
+
+def blurred_img(size=(400, 400)):
+    """
+    Gaussian blur
+    """
+    mat = random_img(size)
+    kernel = cv2.getGaussianKernel(129, 7)
+    mat = cv2.filter2D(mat, cv2.CV_8UC3, kernel)  # convolve
+    mat = np.asarray(mat, dtype=np.int8)
+    return mat
+
+
+if __name__ == "__main__":
+    imshow(random_img())
+    imshow(blurred_img())
